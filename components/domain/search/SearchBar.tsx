cb0951bc4a6aa5d4b1d55bbc085afab439003d16
carrbanog <loopang5954@gmail.com>
2026-06-12 22:06:01 +0900

    검색어 자동완성 추가

---
+++ components/domain/search/SearchBar.tsx
@@ -0,0 +1,92 @@
+"use client";
+
+import { useState, useEffect, useRef } from "react";
+
+import { useRouter } from "next/navigation";
+import { Search } from "lucide-react";
+import { Movie } from "@/type/movie";
+import SearchDropdown from "@/components/domain/search/SearchDropdown";
+
+export default function SearchBar() {
+  const [query, setQuery] = useState("");
+  const [debounceQuery, setDebounceQuery] = useState(""); //검색어 자동완성 쿼리
+  const [isOpen, setIsOpen] = useState(false);
+  const [results, setResults] = useState<Movie[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  
+  const router = useRouter();
+  const dropdownRef = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setDebounceQuery(query);
+    }, 300);
+  }, [query]);
+
+  useEffect(() => {
+    if (!debounceQuery.trim()) {
+      setIsOpen(false);
+      setResults([]);
+      return;
+    }
+
+    const fetchLiveSearch = async () => {
+      try {
+        setIsLoading(true);
+        const res = await fetch(
+          `/api/search?q=${encodeURIComponent(debounceQuery)}`,
+        );
+
+        if (res.ok) {
+          const data = await res.json();
+          console.log("search bar 컴포넌트 테스트", data);
+          setResults(data.results?.slice(0, 5) || []);
+          setIsLoading(false);
+          setIsOpen(true);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchLiveSearch();
+
+  }, [debounceQuery]);
+
+  useEffect(() => {
+    const handleClickOutside = (e: MouseEvent) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(e.target as Node)) {
+        setIsOpen(false);
+      }
+    };
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => document.removeEventListener("mousedown", handleClickOutside);
+  }, []);
+
+  const handleSearchSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (query.trim()) {
+      router.push(`/search?q=${encodeURIComponent(query)}`);
+    }
+    setQuery("");
+  };
+
+  return (
+    <div className="relative max-w-2xl flex items-center w-full " ref={dropdownRef}>
+      <form
+        onSubmit={handleSearchSubmit}
+        className="relative flex w-full max-w-2xl items-center"
+      >
+        <Search className="absolute left-3 h-4 w-4 text-muted-foreground" />
+        <input
+          type="text"
+          placeholder="보고싶은 영화를 검색해보세요"
+          onChange={(e) => setQuery(e.target.value)}
+          value={query}
+          // 자동완성을 띄우기 위한 onFocus 이벤트도 비워두었습니다.
+          className="h-10 w-full rounded-full border bg-secondary/50 pl-10 pr-4 text-sm outline-none transition-colors focus:border-primary focus:bg-background"
+        />
+      </form>
+
+      <SearchDropdown isOpen={isOpen} results={results} isLoading={isLoading} onClose ={() => setIsOpen(false)} />
+    </div>
+  );
+}
---
+++ components/domain/search/SearchDropdown.tsx
@@ -0,0 +1,71 @@
+import { Movie } from "@/type/movie";
+import Image from "next/image";
+import Link from "next/link";
+
+interface SearchDropdownProps {
+  isOpen: boolean;
+  results: Movie[];
+  isLoading: boolean;
+  onClose: () => void;
+}
+
+export default function SearchDropdown({
+  isOpen,
+  results,
+  isLoading,
+  onClose,
+}: SearchDropdownProps) {
+  if (!isOpen) return null;
+
+  console.log("검색어 자동완성", results);
+  return (
+    <div className="absolute left-0 top-12 z-50 w-full overflow-hidden rounded-xl border bg-background shadow-lg p-2">
+      {results.length > 0 ? (
+        <ul className="flex flex-col gap-1">
+          {results.map((movie) => (
+            <li key={movie.id}>
+              <Link
+                href={`/movie/${movie.id}`}
+                // 💡 중요: onMouseDown을 써야 input 포커스가 사라지기 전에 페이지 이동이 먼저 실행됩니다.
+                onMouseDown={(e) => e.preventDefault()}
+                onClick={onClose}
+                className="flex items-center gap-3 rounded-md p-2 hover:bg-muted transition-colors"
+              >
+                {/* 썸네일 영역 */}
+                <div className="relative h-12 w-8 shrink-0 overflow-hidden rounded bg-secondary">
+                  {movie.poster_path ? (
+                    <Image
+                      src={`https://image.tmdb.org/t/p/w92${movie.poster_path}`}
+                      alt={movie.title}
+                      fill
+                      className="object-cover"
+                    />
+                  ) : (
+                    <div className="flex h-full w-full items-center justify-center text-[10px] text-muted-foreground">
+                      No Img
+                    </div>
+                  )}
+                </div>
+                {/* 텍스트 영역 */}
+                <div className="flex flex-col overflow-hidden">
+                  <span className="truncate text-sm font-medium">
+                    {movie.title}
+                  </span>
+                  <span className="text-xs text-muted-foreground">
+                    {movie.release_date
+                      ? movie.release_date.substring(0, 4)
+                      : "미정"}
+                  </span>
+                </div>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <div className="p-4 text-center text-sm text-muted-foreground">
+          일치하는 영화가 없습니다.
+        </div>
+      )}
+    </div>
+  );
+}
