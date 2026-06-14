6550e3a9c182e2fb7eb0a0f526de92216c29a410
carrbanog <loopang5954@gmail.com>
2026-06-14 17:42:07 +0900

    최적화 내용 주석으로 추가

--- components/domain/movie/MovieCard.tsx
+++ components/domain/movie/MovieCard.tsx
@@ -5,27 +5,30 @@ interface MovieCardProps {
   id: string | number;
   title: string;
   poster_path: string;
-  // release_date: string;
+  priority?: boolean;
+  release_date: string;
 }
 
-export default function MovieCard({ id, title, poster_path,}: MovieCardProps) {
+export default function MovieCard({ id, title, poster_path, priority, release_date }: MovieCardProps) {
   return (
     <Link
       href={`/movie/${id}`}
       className="group flex flex-col gap-2 rounded-lg border p-3 shadow-sm transition-transform hover:scale-105"
     >
+      {/* w/342를 적용해서 네트워크 탭에서 682kb로 줄임 */}
       <div className="relative aspect-[2/3] w-full overflow-hidden rounded-md bg-muted">
         <Image
-          src={`https://image.tmdb.org/t/p/w500${poster_path}`}
+          src={`https://image.tmdb.org/t/p/w342${poster_path}`}
           alt={title}
           fill
           sizes="(max-width: 768px) 50vw, (max-width: 1200px) 33vw, 20vw"
           className="object-cover transition-opacity group-hover:opacity-80"
+          priority={priority}
         />
       </div>
       <div className="mt-2 space-y-1">
         <h3 className="line-clamp-1 text-sm font-semibold">{title}</h3>
-        {/* <p className="text-xs text-muted-foreground">{release_date}</p> */}
+        <p className="text-xs text-muted-foreground">{release_date}</p>
       </div>
     </Link>
   );
