a8063c53a8ca0425096235a2e66a7bd7e9630073
carrbanog <loopang5954@gmail.com>
2026-07-15 17:19:53 +0900

    api요청 캐싱 적용

--- services/tmdb.ts
+++ services/tmdb.ts
@@ -35,8 +35,7 @@ export async function getMoviesByGenre(
 ): Promise<Movie[]> {
   const data = await tmdbFetch<MovieResponse>(
     `/discover/movie?with_genres=${genreId}&language=ko-KR&page=${page}&sort_by=popularity.desc&api_key=${API_KEY}`,
-    // { next: { revalidate: 3600 } },
-    { cache: 'no-store' },
+    { next: { revalidate: 3600 } },
     "장르별 영화 목록을 불러오는데 실패했습니다."
   );
   
@@ -46,8 +45,7 @@ export async function getMoviesByGenre(
 export async function getMovieDetail(id: string): Promise<MovieDetailResponse> {
   return tmdbFetch<MovieDetailResponse>(
     `/movie/${id}?language=ko-KR&api_key=${API_KEY}`,
-    // { next: { revalidate: 3600 } },
-    { cache: 'no-store' },
+    { next: { revalidate: 3600 } },
     "영화 상세 정보를 불러오는데 실패했습니다."
   );
 }
