985d3aae4360a94331921ea17ddd03e4d3f62d6e
carrbanog <loopang5954@gmail.com>
2026-07-15 16:22:06 +0900

    React Query Devtools 설치

--- components/domain/auth/AuthProvider.tsx
+++ components/domain/auth/AuthProvider.tsx
@@ -4,6 +4,8 @@
 import { useState } from "react";
 import { SessionProvider } from "next-auth/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+// 1. Devtools 임포트 추가
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 export default function AuthProvider({ children }: { children: React.ReactNode }) {
   // [최적화 측정 포인트] useState를 사용해 SSR 환경에서 
@@ -21,6 +23,9 @@ export default function AuthProvider({ children }: { children: React.ReactNode }
     <SessionProvider>
       <QueryClientProvider client={queryClient}>
         {children}
+        
+        {/* 2. Provider 내부에 Devtools 배치 (처음에는 닫혀있도록 설정) */}
+        <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
     </SessionProvider>
   );
