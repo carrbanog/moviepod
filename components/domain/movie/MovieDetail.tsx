df65ebdeda78fc5d93cdad25a62c4504f7b27eca
carrbanog <loopang5954@gmail.com>
2026-07-20 20:59:59 +0900

    최적화 전

--- components/domain/movie/MovieDetail.tsx
+++ components/domain/movie/MovieDetail.tsx
@@ -1,14 +1,13 @@
 // components/domain/movie/MovieDetailContainer.tsx
-import { Suspense } from "react";
+// import { Suspense } from "react";
 import Image from "next/image";
 import Link from "next/link";
-import { getMovieDetail } from "@/services/tmdb";
-// import { getMovieDetail, getMovieTrailer } from "@/services/tmdb";
-import TrailerSection from "@/components/domain/movie/TrailerSection";
-// import FavoriteButton from "@/components/domain/user/FavoriteButton";
-// import MovieTrailer from "@/components/domain/movie/MovieTrailer";
-// import dynamic from "next/dynamic";
-import FavoriteButtonDynamic from "@/components/domain/user/FavoriteButtonDynamic";
+// import { getMovieDetail } from "@/services/tmdb";
+import { getMovieDetail, getMovieTrailer } from "@/services/tmdb";
+// import TrailerSection from "@/components/domain/movie/TrailerSection";
+import FavoriteButton from "@/components/domain/user/FavoriteButton";
+import MovieTrailer from "@/components/domain/movie/MovieTrailer";
+// import FavoriteButtonDynamic from "@/components/domain/user/FavoriteButtonDynamic";
 
 interface MovieDetailContainerProps {
   id: string;
@@ -17,12 +16,12 @@ interface MovieDetailContainerProps {
 export default async function MovieDetailContainer({
   id,
 }: MovieDetailContainerProps) {
-  // const [movie, trailerKey] = await Promise.all([
-  //   getMovieDetail(id),
-  //   getMovieTrailer(id),
-  // ]);
+  const [movie, trailerKey] = await Promise.all([
+    getMovieDetail(id),
+    getMovieTrailer(id),
+  ]);
 
-  const movie = await getMovieDetail(id);
+  // const movie = await getMovieDetail(id);
   console.log("영화 상세 정보", movie);
   return (
     <article className="flex flex-col gap-12 w-full">
@@ -44,9 +43,9 @@ export default async function MovieDetailContainer({
           <h1 className="text-4xl font-bold">{movie.title}</h1>
 
           <div>
-            <FavoriteButtonDynamic movie={movie} />
+            {/* <FavoriteButtonDynamic movie={movie} /> */}
             
-            {/* <FavoriteButton movie={movie} /> */}
+            <FavoriteButton movie={movie} />
           </div>
 
           {movie.tagline && (
@@ -82,7 +81,7 @@ export default async function MovieDetailContainer({
 
       <section>
         <h2 className="text-2xl font-bold mb-4">🎬 메인 예고편</h2>
-        {/* {trailerKey ? (
+        {trailerKey ? (
           <MovieTrailer trailerKey={trailerKey} movieTitle={movie.title} />
         ) : (
           <div className="flex flex-col items-center justify-center rounded-xl border border-dashed bg-muted/40 py-12 text-center">
@@ -93,14 +92,14 @@ export default async function MovieDetailContainer({
               다른 언어나 관련 영상 리소스가 존재하지 않는 영화입니다.
             </p>
           </div>
-        )} */}
-        <Suspense
+        )}
+        {/* <Suspense
           fallback={
             <div className="h-[300px] w-full bg-muted animate-pulse rounded-xl" />
           }
         >
           <TrailerSection id={id} movieTitle={movie.title} />
-        </Suspense>
+        </Suspense> */}
       </section>
     </article>
   );
